a5608f3eb6e7c2169d9327650e329681250064f9
ran <ran@goz.com>
2023-08-04 00:29:36 +0300

    added some flights to db + added flight to admin + fixed the templates a bit

--- flight/admin.py
+++ flight/admin.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from django.contrib import admin
-# from .models import Flight, User, SoundList, Sound
+from .models import Flight
 from django.contrib.auth.admin import UserAdmin
 from .models import MyUser
 
 admin.site.register(MyUser)
+admin.site.register(Flight)
 
 
 
--- flights_website/urls.py
+++ flights_website/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('flight/', include('flight.urls')),
+    path('', include('flight.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
